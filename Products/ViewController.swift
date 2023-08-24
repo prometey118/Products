76962be52ce4c9f7dba74d3bbe23c0d584a92846
Yaroslav <neptun2001yarik@gmail.com>
2023-08-24 23:29:24 +0300

    add collection view

--- Products/ViewController.swift
+++ Products/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    var collectionView: UICollectionView!
+    let temp = ["cat", "cat", "cat", "cat", "cat"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
+        
     }
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+        cell.catImageView.image = UIImage(named: temp[indexPath.row])
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width/2 - 20, height: 250)
+    }
+    
+    class CustomCell: UICollectionViewCell {
+        let catImageView = UIImageView()
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            addSubview(catImageView)
+            
+            
+            catImageView.translatesAutoresizingMaskIntoConstraints = false
+            catImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+            catImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+            catImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+            catImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
 
 }
 
